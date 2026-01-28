5f2fc9af63441336a9582de30371dec2b8c40c44
Anurag Kumar <anuragkumar666333@gmail.com>
2026-01-28 18:02:44 +0530

    controler and routes setup

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 500;
 
+app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoute"));
-
+app.use(errorHandler)
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 });
