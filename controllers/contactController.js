64e858868f0f7f279243cba13d306c09d1c6fb50
Anurag Kumar <anuragkumar666333@gmail.com>
2026-01-31 14:47:04 +0530

    finished

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,16 +4,16 @@ const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET/api/contacts
-//access public 
+//access private 
 
 const getContacts = asyncHandler(async(req,res)=>{
-    const contact =  await Contact.find();
+    const contact =  await Contact.find({ user_id : req.user.id});
     res.status(200).json(contact);
 })
 
 //@desc Create New contacts
 //@route POST/api/contacts
-//access public 
+//access private 
 
 const createContact =  asyncHandler(async(req,res)=>{
     console.log("The req body :", req.body);
@@ -23,14 +23,14 @@ const createContact =  asyncHandler(async(req,res)=>{
         throw new Error("All fields are mandatory !");
     }
     const contact = await Contact.create({
-        name,email,phone,
+        name,email,phone,user_id : req.user.id
     });
     res.status(201).json(contact);
 })
 
 //@desc Get contact
 //@route GET/api/contacts/:id
-//access public 
+//access private  
 
 const getContact = asyncHandler(async(req,res)=>{
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
@@ -47,17 +47,23 @@ const getContact = asyncHandler(async(req,res)=>{
 
 //@desc Update contact
 //@route UPDATE/api/contacts/:id
-//access public 
+//access private 
 
 const updateContact = asyncHandler(async(req,res)=>{
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
        res.status(400);
        throw new Error("Invalid contact ID");
     }
+
     const contact = await Contact.findById(req.params.id);
     if(!contact){
         res.status(404);
         throw new Error("Contact not Found");
+    }
+
+    if(contact.user_id.toString() != req.user.id){
+        res.status(403);
+        throw new Error("User don't have the permission to update other user contacts");
     }
 
     const updateContact = await Contact.findByIdAndUpdate(
@@ -70,7 +76,7 @@ const updateContact = asyncHandler(async(req,res)=>{
 
 //@desc Delete contact
 //@route DELETE/api/contacts/:id
-//access public 
+//access private 
 
 const deleteContact = asyncHandler(async(req,res)=>{
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -2,22 +2,22 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async(req,res,next)=>{
-    let token;
     let authHeader = req.headers.Authorization || req.headers.authorization;
-    if(authHeader && authHeader.startsWith("Bearer")){
-        token = authHeader.split(" ")[1];
-        jwt.verify(token , process.env.ACCESS_TOKEN_SECRET,(err,decode)=>{
-            if(err){
-                res.status(401);
-                throw new Error("User is not authorized");
-            }
-            req.user = decode.user;
-            next();
-        });
-    }else{
-        res.status(401);
-        throw new Error("User is not authorized or token is missing");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401);
+    throw new Error("User is not authorized or token is missing");
     }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded.user;
+    next();
+    }catch (error) {
+    res.status(401);
+    throw new Error("Invalid or expired token");
+  }
 });
 
 module.exports = validateToken;
